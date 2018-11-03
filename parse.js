8195dbd8b50273c803c0ae0982d384c15eda17b9
astieber <astieber@taittowers.com>
2018-11-03 15:05:49 -0600

    Cleaned up code, added equation display with basic coloring.

--- parse.js
+++ parse.js
@@ -1,99 +1,112 @@
-var SINGLE_OPERATORS = ['~', '!'];
-var DOUBLE_OPERATORS = ['&', '&&', '|', '||', '<<', '>>', '>>>', '^'];
+var UNARY_OPERATORS = ['~', '!'];
+var BINARY_OPERATORS = ['&', '&&', '|', '||', '<<', '>>', '>>>', '^'];
 var NOT_AVAILABLE = "N/A";
 var DECIMAL_REGEX = new RegExp(/-?\d+/);
 //var HEX_REGEX = /0xblahblah/;
+
 
 function onKeyTyped() {
   var currentText = document.getElementById('textbox').value;
 
   var argList = parseCurrentText(currentText);
 
-  var isSolvable = true;
-  isSolvable &= areArgsValid(argList);
-  isSolvable &= isOperatorValid(argList);
+  var isSolvable = areArgsValid(argList) && isOperatorValid(argList);
 
-  updateResultSpan(argList, isSolvable);
+  updateResultDiv(argList, isSolvable);
 
 }
 
 function parseCurrentText(currentText) {
-  return currentText.split(' ').filter(val => val.length != 0); //remove empty strings
+  return currentText.split(' ').filter(val => val.length !== 0); //remove empty strings
 }
 
 function areArgsValid(argList) {
   if (argList.length === 2 && DECIMAL_REGEX.test(argList[1])) {
-    argCountSpan.textContent = "{" + argList[1] + "}";
+    argDebugSpan.textContent = `{${argList[1]}}`; //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
     return true;
   }
   if (argList.length === 3 && DECIMAL_REGEX.test(argList[0]) && DECIMAL_REGEX.test(argList[2])) {
-    argCountSpan.textContent = "{" + argList[0] + ", " + argList[2] + "}";
+    argDebugSpan.textContent = `{${argList[0]}, ${argList[2]}}`;
     return true;
   }
   
-  argCountSpan.textContent = NOT_AVAILABLE;
+  argDebugSpan.textContent = NOT_AVAILABLE;
   return false;
 }
 
 function isOperatorValid(argList) {
-  if (argList.length === 2 && $.inArray(argList[0], SINGLE_OPERATORS) > -1) { //$.inArray() is JQuery
-    operatorSpan.textContent = argList[0];
+  if (argList.length === 2 && $.inArray(argList[0], UNARY_OPERATORS) > -1) { //$.inArray() is JQuery
+    operatorDebugSpan.textContent = argList[0];
     return true;
   }
-  else if (argList.length === 3 && $.inArray(argList[1], DOUBLE_OPERATORS) > -1) {
-    operatorSpan.textContent = argList[1];
+  if (argList.length === 3 && $.inArray(argList[1], BINARY_OPERATORS) > -1) {
+    operatorDebugSpan.textContent = argList[1];
     return true;
   }
   
-  operatorSpan.textContent = NOT_AVAILABLE;
+  operatorDebugSpan.textContent = NOT_AVAILABLE;
   return false;
 }
 
-function updateResultSpan(argList, isSolvable) {
+function updateResultDiv(argList, isSolvable) {
   if (isSolvable) {
-    if (argList.length === 2) {
-      var firstInt = parseInt(argList[1]);
-      switch (argList[0]) {
-        case '~':
-          resultSpan.textContent = ~firstInt;
-          break;
-        case '!':
-          resultSpan.textContent = !firstInt;
-          break;
-      }
+    var result, firstInt, secondInt, operator;
+    if (argList.length < 3) { //unary
+      operator = argList[0];
+      firstInt = ""; //hide first 
+      secondInt = parseInt(argList[1]);
+      result = findUnarySolution(operator, secondInt);
     }
-    else if (argList.length === 3) {
-      var firstInt = parseInt(argList[0]);
-      var secondInt = parseInt(argList[2]);
+    else { //binary
+      operator = argList[1];
+      firstInt = parseInt(argList[0]);
+      secondInt = parseInt(argList[2]);
+      result = findBinarySolution(operator, firstInt, secondInt);
+    }
 
-      switch (argList[1]) {
-        case '&': 
-          resultSpan.textContent = firstInt & secondInt;
-          break;
-        case '&&': 
-          resultSpan.textContent = firstInt && secondInt;
-          break;
-        case '|': 
-          resultSpan.textContent = firstInt | secondInt;
-          break;
-        case '||': 
-          resultSpan.textContent = firstInt || secondInt;
-          break;
-        case '<<': 
-          resultSpan.textContent = firstInt << secondInt;
-          break;
-        case '>>': 
-          resultSpan.textContent = firstInt >> secondInt;
-          break;
-        case '>>>': 
-          resultSpan.textContent = firstInt >>> secondInt;
-          break;
-        case '^':
-          resultSpan.textContent = firstInt ^ secondInt;
-          break;
-      }
-    }
+    operatorSpan.textContent = operator;
+
+    firstIntSpan.textContent = firstInt; firstIntSpan.setAttribute('truthy', String(!!firstInt));
+    secondIntSpan.textContent = secondInt; secondIntSpan.setAttribute('truthy', String(!!secondInt));
+    resultSpan.textContent = result; resultSpan.setAttribute('truthy', String(!!result));
+    
+    resultDiv.style.display = 'block';
   }
+  else {
+    resultDiv.style.display = 'none';
+  }
+}
 
-  else resultSpan.textContent = NOT_AVAILABLE;
+function findUnarySolution(operatorString, firstInt) {
+  switch (operatorString) {
+    case '~':
+      return ~firstInt;
+    case '!':
+      return !firstInt;
+    default:
+      throw `Unary Parsing Failed. Op: {${operatorString}}. Arg: {${firstInt}}`;
+  }
+}
+
+function findBinarySolution(operatorString, firstInt, secondInt) {
+  switch (operatorString) {
+    case '&': 
+      return firstInt & secondInt;
+    case '&&': 
+      return firstInt && secondInt;
+    case '|': 
+      return firstInt | secondInt;
+    case '||': 
+      return firstInt || secondInt;
+    case '<<': 
+      return firstInt << secondInt;
+    case '>>': 
+      return firstInt >> secondInt;
+    case '>>>': 
+      return firstInt >>> secondInt;
+    case '^':
+      return firstInt ^ secondInt;
+    default:
+      throw `Binary Parsing Failed. Op: {${operatorString}}. Args: {${firstInt}, ${secondInt}}`;
+  }
 }
