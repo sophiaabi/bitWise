9981a66e323b63e286d88b356a8c6eb550fead5d
Tiffany Phan <Tiffany.Phan@colorado.edu>
2018-11-14 18:40:43 -0700

    heroku again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 var express = require('express');
 var app = express();
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 var expressValidator = require('express-validator');
 app.use(expressValidator());
